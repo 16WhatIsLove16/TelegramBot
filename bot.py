d22be8caee7db4340ea2327f039ca9a922f53329
andre <andreev.vlad100@yandex.ru>
2024-09-25 11:36:18 +0300

    second_commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from datetime import datetime
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.utils.formatting import Text, Bold
 from aiogram.types import Message
@@ -23,7 +23,10 @@ dp['started_at'] = datetime.now().strftime("%d.%m.%Y %H:%M")
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer("Привет! Я - многофункциональный бот, который умеет много разных штук.\n \n" \
+                         "<b>Команды:</b> \n/start" \
+                         "\n/info" \
+                         "\n/hello", parse_mode='HTML')
 
 
 # Хэндлер на команду /hello
@@ -31,9 +34,100 @@ async def cmd_start(message: types.Message):
 async def cmd_hello(message: Message):
     content = Text(
         "Привет, ",
-        Bold(message.from_user.full_name)
+        Bold(message.from_user.full_name),
+        "!\nКак твоё настроение?"
     )
-    await message.answer(**content.as_kwargs())
+
+    kb = [
+        [
+            types.KeyboardButton(text="Хорошо"),
+            types.KeyboardButton(text="Плохо")
+        ],
+    ]
+
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Только честно)"
+    )
+
+    await message.answer(**content.as_kwargs(), reply_markup=keyboard)
+
+
+# Кнопка "хорошо" в хэндлере /hello
+@dp.message(F.text.lower() == "хорошо")
+async def good(message: types.Message):
+
+    kb = [
+        [
+            types.KeyboardButton(text="Да, давай"),
+            types.KeyboardButton(text="Пожалуй, не сегодня")
+        ],
+    ]
+
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True
+    )
+
+    await message.reply("Прекрасно, могу рассказать тебе анекдот, хочешь?", reply_markup=keyboard)
+
+
+# Кнопка "плохо" в хэндлере /hello
+@dp.message(F.text.lower() == "плохо")
+async def bad(message: types.Message):
+
+    kb = [
+        [
+            types.KeyboardButton(text="Да, давай"),
+            types.KeyboardButton(text="Пожалуй, не сегодня")
+        ],
+    ]
+
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True
+    )
+
+    await message.reply("Оу, хочешь, я расскажу тебе анекдот?", reply_markup=keyboard)
+
+
+# Отображение анекдота
+@dp.message(F.text.lower() == "да, давай")
+async def yes(message: Message):
+    content = Text("Какой-то анекдот")
+
+    kb = [
+        [
+            types.KeyboardButton(text="Давай ещё"),
+            types.KeyboardButton(text="Достаточно")
+        ],
+    ]
+
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True
+    )
+
+    await message.answer(**content.as_kwargs(), reply_markup=keyboard)
+
+
+# Завершение цикла отображения анекдотов до отображения анекдотов
+@dp.message(F.text.lower() == "пожалуй, не сегодня")
+async def not_today(message: types.Message):
+    await message.reply("Хорошо", reply_markup=types.ReplyKeyboardRemove())
+
+
+# Продолжение цикла отображения анекдотов
+@dp.message(F.text.lower() == "давай ещё")
+async def more(message: types.Message):
+    await yes(message)
+
+
+# Завершение цикла отображения анекдотов после отображения анекдотов
+@dp.message(F.text.lower() == "достаточно")
+async def enought(message: Message):
+    await message.reply("Хорошо", reply_markup=types.ReplyKeyboardRemove())
 
 
 # Хэндлер на команду /info
